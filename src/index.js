1432a580c33c5be89c99c027a36c4f463ae4514b
VINEETH DASARI <dasariwineethoo7@gmail.com>
2024-06-10 16:27:17 +0530

    first commmit basic setup

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+
+const {PORT} = require('./config/serverConfig');
+const apiroutes = require('./routes/index');
+const app = express();
+const db = require('./models/index');
+
+const setupAndStartServer = () => {
+
+    app.use('/api', apiroutes);
+    app.use(bodyparser.json());
+    app.use(bodyparser.urlencoded({extended : true}));
+
+    app.listen(PORT, () => {
+        console.log('Server Started on Port ${PORT}');
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter : true});
+        }
+    })
+}
+
+setupAndStartServer();
